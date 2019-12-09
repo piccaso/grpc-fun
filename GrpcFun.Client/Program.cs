d2c734b2eeb3f8f559332f76f3719596482eb8ee
FF <@>
2019-12-09 17:01:15 +0100

    update client

--- GrpcFun.Client/Program.cs
+++ GrpcFun.Client/Program.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Net.Http;
+using Grpc.Core;
 using Grpc.Net.Client;
 using GrpcFun.Server;
 
 namespace GrpcFun.Client {
     class Program {
         static void Main(string[] args) {
-            using var httpClientHandler = new HttpClientHandler();
-            httpClientHandler.ServerCertificateCustomValidationCallback += (message, certificate2, arg3, arg4) => true;
-            using var httpClient = new HttpClient(httpClientHandler);
-            var options = new GrpcChannelOptions {HttpClient = httpClient};
-            using var channel = GrpcChannel.ForAddress("https://m.887.at:65211/", options);
+            using var channel = GrpcChannel.ForAddress("https://grpc-fun.m.887.at/");
             var client = new Greeter.GreeterClient(channel);
             
             var world = client.SayHello(new HelloRequest{Name = "world"});
@@ -19,14 +15,14 @@ namespace GrpcFun.Client {
             try {
                 var pete = client.SayHello(new HelloRequest{Name = "pete"});
                 Console.WriteLine(pete.Message);
-            } catch (Exception e) {
+            } catch (RpcException e) {
                 Console.WriteLine(e.Message);
             }
 
             try {
                 var rumpel = client.SayHello(new HelloRequest{Name = "rumpelstilzchen"});
                 Console.WriteLine(rumpel.Message);
-            } catch (Exception e) {
+            } catch (RpcException e) {
                 Console.WriteLine(e.Message);
             }
 
